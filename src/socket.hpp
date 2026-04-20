0bfefea7f7ecba677d4b9a8b79d33b6767fcd902
Paul Overbeck <paul.overbeck95@gmail.com>
2026-04-20 17:42:58 +0200

    mini server'

---
+++ src/socket.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <exception>
+#include <algorithm>
+
+#include <vector>
+
+#include <netinet/in.h> 
+#include <sys/socket.h> 
+#include <unistd.h>
