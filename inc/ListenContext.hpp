5ac91d7c4cebe5f2d92049ffaba25d88e733be95
Paul <paul.overbeck95@gmail.com>
2026-05-08 12:55:03 +0200

    spaces

--- inc/ListenContext.hpp
+++ inc/ListenContext.hpp
@@ -4,16 +4,6 @@
 #include <vector>
 #include "ServerConfig.hpp"
 
-//struct ListenContext {
-//    int 						_fd;				// socket_fd
-//    std::string					_host;				// bind adress
-//    int							_port;				// bind adress
-//    std::vector<ServerConfig*>	_candidates;		// alle moeglichen server_configs (gleicher port)
-//    ServerConfig*				_default_server;	// erster server_block in _candidates
-//};
-
-
-
 struct ListenContext {
     int 						_fd;				// socket_fd
     std::string					_host;				// bind adress
