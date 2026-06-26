3c9c83c006ee6ced8947afb0364bae4782ee5361
ghambrec <gabriel.hambrecht@gmail.com>
2026-06-26 17:48:17 +0200

    added bbqserv folder

---
+++ bbqserv/cgi/greet.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import os
+import sys
+from urllib.parse import parse_qs
+
+method = os.environ.get("REQUEST_METHOD", "GET")
+
+if method == "GET":
+    query = os.environ.get("QUERY_STRING", "")
+    params = parse_qs(query)
+    name = params.get("name", ["Welt"])[0]
+else:
+    length = int(os.environ.get("CONTENT_LENGTH", "0"))
+    body = sys.stdin.read(length)
+    params = parse_qs(body)
+    name = params.get("name", ["Welt"])[0]
+
+body_html = f"<html><body><h1>Hallo {name}!</h1></body></html>"
+
+print("Content-Type: text/html")
+print("Status: a204 No content")
+print(f"Content-Length: {len(body_html)}")
+print()
+print(body_html, end="")
