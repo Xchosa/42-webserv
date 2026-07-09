0214651d64ac7d054212ca2f60526014f6be270d
ghambrec <gabriel.hambrecht@gmail.com>
2026-07-09 13:20:56 +0200

    cleaned and better logging for ClientInfos selectVirtualHost

--- src/server/ClientInfos.cpp
+++ src/server/ClientInfos.cpp
@@ -9,28 +9,17 @@ void ClientInfos::selectVirtualHost()
 	{
 		if (headerName == "host")
 		{
-			// std::cout << "header key   = [" << headerName << "]\n";
-			// std::cout << "header value = [" << headerValue << "]\n";
-			
 			for(size_t i = 0; i < _listen_context->_candidates.size(); ++i)
 			{
 				ServerConfig *tmpServer = _listen_context->_candidates[i];
 				if (tmpServer == nullptr)
-				{
-					// std::cout << "  tmpServer is NULL" << std::endl;
 					continue;
-				}
-
-				// std::cout << "  listen host: " << tmpServer->_listen_host << std::endl;
-				// std::cout << "  listen port: " << tmpServer->_listen_port << std::endl;
-				// std::cout << "  listen server: " <<  std::endl;
 				for (size_t j = 0; j < tmpServer->_server_names.size(); ++j)
 				{
-					// std::cout << "    [" << tmpServer->_server_names[j] << "]" << std::endl;
 					if(headerValue == tmpServer->_server_names[j])
 					{
-						// std::cout << "match found: " <<  tmpServer->_server_names[j] <<std::endl;
 						_selected_server = tmpServer;
+						std::cout << "[INFO]  virtual host match: '" << tmpServer->_server_names[j] << "'" << std::endl;
 						break;
 					}
 				}
@@ -39,8 +28,8 @@ void ClientInfos::selectVirtualHost()
 	}
 	if (_selected_server == nullptr)
 	{
-		// std::cout << "kein server gefunden, nehme default\n";
 		_selected_server = _listen_context->_default_server;
+		std::cout << "[INFO]  no virtual host match, take default server: '" << _listen_context->_default_server->_server_names[0] << "'" << std::endl;
 	}
 	_parser.setServerConfig(_selected_server);
 }
